8ba206199f0ac0498779f9cbfe60b873a59b1de3
Canh Doan <doanc@saic.com>
2019-10-22 16:42:55 -0400

    Added menu with tabs.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+### How to Combine Ionic 4 Tabs and Side Menu Navigation
+
+https://www.youtube.com/watch?v=4Li_FDCGi7E
+
 ionic start ionic4-app tabs
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+    { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule' },
+    // { path: 'first-with-tabs', loadChildren: './pages/first-with-tabs/first-with-tabs.module#FirstWithTabsPageModule' },
+    // { path: 'second', loadChildren: './pages/second/second.module#SecondPageModule' },
+    // { path: 'tab1', loadChildren: './pages/tab1/tab1.module#Tab1PageModule' },
+    // { path: 'tab2', loadChildren: './pages/tab2/tab2.module#Tab2PageModule' },
+    // { path: 'details', loadChildren: './pages/details/details.module#DetailsPageModule' }
 ];
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    ],
+    exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
